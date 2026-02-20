e3ae3a853acb7f281e4f9392570d62f36edb3113
khalidbih <kain19m@hotmail.com>
2026-02-20 22:28:50 +0000

    Fix error caused by using const before declaration

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,6 @@
 // Currently trying to print the string "I was born in Bolton" but it isn't working...
 // what's the error ?
-
-console.log(`I was born in ${cityOfBirth}`);
+// The error happens because cityOfBirth is used before it is declared,
+// Variables declared with const cannot be accessed before their declaration.
 const cityOfBirth = "Bolton";
+console.log(`I was born in ${cityOfBirth}`);
