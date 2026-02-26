68d57cef6a56a070663b0e72971d22f312fcf5f2
khalidbih <kain19m@hotmail.com>
2026-02-26 22:36:15 +0000

    correct convertToPercentage function

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,20 +1,27 @@
 // Predict and explain first...
+// The code will give an error because `decimalNumber` is declared twice, 
+// once as the function parameter, and again with `const` inside the function.
 
 // Why will an error occur when this program runs?
 // =============> write your prediction here
-
+// The error occurs because `decimalNumber` has already been declared.
 // Try playing computer with the example to work out what is going on
 
-function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
-  const percentage = `${decimalNumber * 100}%`;
+// function convertToPercentage(decimalNumber) {
+// const decimalNumber = 0.5;
+// const percentage = `${decimalNumber * 100}%`;
 
-  return percentage;
-}
-
-console.log(decimalNumber);
+// return percentage;
+// }
+// console.log(decimalNumber);
 
 // =============> write your explanation here
-
+// When I run the code, it gives a SyntaxError saying that, `decimalNumber` has already been declared.
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+
+function convertToPercentage(decimalNumber){
+  const percentage = `${decimalNumber * 100}%`;
+  return percentage
+}
+console.log(convertToPercentage(0.5));
