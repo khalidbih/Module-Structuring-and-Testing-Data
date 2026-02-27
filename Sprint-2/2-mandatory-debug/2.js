ddee2257346027ffb397316d15ab8776422b4973
khalidbih <kain19m@hotmail.com>
2026-02-27 23:45:40 +0000

    fix getLastDigit function to use parameter

--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -2,10 +2,34 @@
 
 // Predict the output of the following code:
 // =============> Write your prediction here
+// The output will always be 3.This is because the function uses the variable `num`,
+// which is set to 103.
+// const num = 103;
 
-const num = 103;
+// function getLastDigit() {
+// return num.toString().slice(-1);
+// }
 
-function getLastDigit() {
+// console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+// console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+// console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+
+// Now run the code and compare the output to your prediction
+// =============> write the output here
+
+// The last digit of 42 is 3
+// The last digit of 105 is 3
+// The last digit of 806 is 3
+
+// Explain why the output is the way it is
+// =============> write your explanation here
+
+// The output is always 3 because the function uses `num` (103),and ignores the number passed into it.
+// Finally, correct the code to fix the problem
+
+// =============> write your new code here
+
+function getLastDigit(num) {
   return num.toString().slice(-1);
 }
 
@@ -13,12 +37,8 @@ console.log(`The last digit of 42 is ${getLastDigit(42)}`);
 console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
 
-// Now run the code and compare the output to your prediction
-// =============> write the output here
-// Explain why the output is the way it is
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-// =============> write your new code here
+
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
+// We must pass the number into the function as a parameter.
