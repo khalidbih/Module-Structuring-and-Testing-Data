0ee89c83bbab05ea470ad49c4ab89cd9369efbc1
khalidbih <kain19m@hotmail.com>
2026-03-02 14:19:42 +0000

    Add toUpperSnakeCase function using replaceAll and toUpperCase

--- Sprint-2/3-mandatory-implement/2-cases.js
+++ Sprint-2/3-mandatory-implement/2-cases.js
@@ -14,3 +14,12 @@
 // You will need to come up with an appropriate name for the function
 // Use the MDN string documentation to help you find a solution
 // This might help https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+
+
+function toUpperSnakeCase(inputString) {
+   return inputString.replaceAll(" ", "_").toUpperCase();
+
+  
+}
+console.log(toUpperSnakeCase("hello there"));
+console.log (toUpperSnakeCase("lord of the rings"));
