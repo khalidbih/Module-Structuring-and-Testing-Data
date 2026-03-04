42be3ff00085b8eeb5d1e63ea9126f6d51ce8ca8
khalidbih <kain19m@hotmail.com>
2026-03-04 22:49:27 +0000

    Implement getOrdinalNumber function with edge case handling

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,19 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  const lastTwoDigits = num % 100;
+  if (lastTwoDigits >= 11 && lastTwoDigits <= 13){
+    return num+ "th";
+  }
+  const lastDigit = num %10;
+  switch (lastDigit){
+    case 1:
+      return num + "st";
+    case 2:
+      return num + "nd";
+    case 3:
+      return num + "rd";
+    default:
+      return num + "th";
+  }
 }
 
 module.exports = getOrdinalNumber;
