4483f9349af94aed7f227dd1e32af2a7580a0788
khalidbih <kain19m@hotmail.com>
2026-03-16 00:57:56 +0000

    Add Jest tests for getCardValue including ace, number, face, and invalid cards

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -13,8 +13,20 @@ test(`Should return 11 when given an ace card`, () => {
 //   Number Cards (2-10)
 //   Face Cards (J, Q, K)
 //   Invalid Cards
+test("should return the value of number cards", () => {
+  expect(getCardValue("2♣")).toEqual(2);
+  expect(getCardValue("5♠")).toEqual(5);
+  expect(getCardValue("10♦")).toEqual(10);
+});
+test("should return 10 for face cards", () => {
+  expect(getCardValue("J♣")).toEqual(10);
+  expect(getCardValue("Q♥")).toEqual(10);
+  expect(getCardValue("K♠")).toEqual(10);
+});
+test("should throw an error for invalid card", () => {
+  expect(() => getCardValue("1♣")).toThrow("invalid card rank.");
+});
 
 // To learn how to test whether a function throws an error as expected in Jest,
 // please refer to the Jest documentation:
 // https://jestjs.io/docs/expect#tothrowerror
-
